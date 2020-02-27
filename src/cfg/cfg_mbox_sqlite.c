42fe158ed032fb26433ba0852cc93ebe20294250
Alex <alex-v@vr5.epicgamer.org>
2020-02-27 03:20:00 +0300

    bugfix - wrong option name

--- src/cfg/cfg_mbox_sqlite.c
+++ src/cfg/cfg_mbox_sqlite.c
@@ -10,7 +10,7 @@ int interpret_cfg_data_SqliteEnable(strValues *val, strOptionsRealm *opts) {
 void dump_cfg_data_SqliteEnable(FILE *fd, strOptionsRealm *opts) {
   if(!fd)
     return;
-  fprintf(fd,"mbox_cache = %d\n",opts->sqlite_enable);
+  fprintf(fd,"enable_sqlite = %d\n",opts->sqlite_enable);
 }
 
 int add_realm_SqliteEnable(strOptionsRealm *dst, strOptionsRealm *src) {
